2b9cc1e3ff8f131769d62be156e95ddc8d5ca2e4
antonmiazyn <antonmiazyn@mail.ru>
2020-03-04 15:17:02 +0200

    Updates

---
+++ precache-manifest.583f810de09dbb7eadf12fd1d07b7908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbd56ea36352ef31332361512330c26",
+    "url": "/prod4.articles/index.html"
+  },
+  {
+    "revision": "b9fdf669fd8d6127a44e",
+    "url": "/prod4.articles/static/css/main.eb408f40.chunk.css"
+  },
+  {
+    "revision": "8ef5d1e4d703f809df24",
+    "url": "/prod4.articles/static/js/2.f58bddd3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/prod4.articles/static/js/2.f58bddd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9fdf669fd8d6127a44e",
+    "url": "/prod4.articles/static/js/main.a9c47f4d.chunk.js"
+  },
+  {
+    "revision": "e143fa3ee7f4172e6fec",
+    "url": "/prod4.articles/static/js/runtime-main.e315cc93.js"
+  }
+]);
